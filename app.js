6cd12fc3b7210356c6fcb0832cbc566c59cf253c
Fagner <fagner.vieira@apis3.com>
2022-04-26 09:49:57 -0300

    teste

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var adminRouter = require('./routes/admin');
 
-var app = express();
+var app = express(); 
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
