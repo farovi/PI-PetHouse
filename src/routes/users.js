2c2560b05bf849426cdd504ecd5fcb2311a28f0d
timeware <timeware>
2022-04-26 16:02:08 -0300

    criando rotas express/adicionando Pagina de login e Checkout

---
+++ src/routes/users.js
@@ -0,0 +1,30 @@
+var express = require("express");
+var router = express.Router();
+
+router.get("/", (req, res) => {
+  res.render("pages/home");
+});
+router.get("/checkout", (req, res) => {
+  res.render("pages/checkout");
+});
+
+router.get("/meuCarrinho", (req, res) => {
+  res.render("pages/meuCarrinho");
+});
+router.get("/produtoInterna", (req, res) => {
+  res.render("pages/produtoInterna");
+});
+router.get("/produtoInterna", (req, res) => {
+  res.render("pages/produtoInterna");
+});
+router.get("/minhaConta", (req, res) => {
+  res.render("pages/minhaConta");
+});
+router.get("/finalizado", (req, res) => {
+  res.render("pages/pedidoFinalizado");
+});
+router.get("/login", (req, res) => {
+  res.render("pages/login");
+});
+
+module.exports = router;
