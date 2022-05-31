8c3e5ee1cb6db7f1b3a77eb2f1693aa8b45f9b57
Naiara Raiane <nakamendes123@gmail.com>
2022-05-31 18:52:00 -0300

    fix adm.cliente

--- src/routes/clienteRoutes.js
+++ src/routes/clienteRoutes.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const clienteController = require('../controllers/clienteController');
 
-router.get('/cliente', clienteController.list);
-router.get('/cliente/detail/:id', clienteController.detail);
+router.get('/cliente',clienteController.list);
+router.get('/cliente/detail/:id',clienteController.detail);
 
 
 module.exports = router;
