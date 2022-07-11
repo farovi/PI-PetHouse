2d33d188fd2153ba0ef216afca239d2158299b7e
lanasoliv <halana.oliveira@hotmail.com>
2022-07-11 10:31:36 -0300

    Marcos - arrumar controller/index

--- src/controllers/produtosController.js
+++ src/controllers/produtosController.js
@@ -1,6 +1,22 @@
-const sequelize = require('sequelize')
+
+const sequelize = require("../../models/db");
+const DataTypes = require("sequelize");
+const Produtos = require("../../models/Produtos")(sequelize, DataTypes);
+const db = require('../../models/index.js');
 
 const produtosController = {
+  index: async (req, res) => {
+    const produtos = await Produtos.findAll();
+    res.json(produtos);
+  }};
+
+module.exports = produtosController;
+
+
+
+
+
+/*const produtosController = {
     index: (req, res) => {
       db.Produtos.findAll().then((data) => {
         res.send(JSON.stringify(data));
