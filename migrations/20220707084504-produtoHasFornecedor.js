7bbc1e725df5b389535141242ce844eb5bae5c77
lanasoliv <halana.oliveira@hotmail.com>
2022-07-11 20:25:10 -0300

    Somente arrumando uma palavra que estava escrito errada

--- migrations/20220707084504-produtoHasFornecedor.js
+++ migrations/20220707084504-produtoHasFornecedor.js
@@ -25,7 +25,7 @@ module.exports = {
 
   async down (queryInterface, Sequelize) {
    
-      await queryInterface.dropTable('produtoHasPedido');
+      await queryInterface.dropTable('produtoHasFornecedor');
      
   }
 }; 
